303d5a92e7c0b5ea9e879fb46f5aefa23ee03b9f
Marcelo Briones <ing@marcelobriones.com.ar>
2022-03-26 01:16:16 -0300

    Add tabs and buttons

---
+++ assets/js/buttons.js
@@ -0,0 +1,9 @@
+$('#random-btn').click(() => {
+  console.log('random clicked');
+});
+$('#default-btn').click(() => {
+  console.log('default clicked');
+});
+$('#create-btn').click(() => {
+  console.log('create clicked');
+});
